b5f9fdec91e0770030732525260e73cb8f153329
Erik Weibust <erik@weibust.net>
2016-10-11 11:40:36 -0500

    Update helloworld.java
    This Java hello world had three issues. I've fixed all three.
    1. The public class name has to match the file name so I changed the classname to helloworld
    2. The static keyword was misspelled
    3. The java main method requires a String array param not a string (not a Java class).
    
    Now you can run javac on helloworld.java and get a valid .class file you can then run with java helloworld.
    
    Erik

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
-public class YourName {
-  public ststic void main(string[] args) {
+public class helloworld {
+  public static void main(String[] args) {
   
   System.out.println("Hello World");
   
