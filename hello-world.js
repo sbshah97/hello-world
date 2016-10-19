f0cde65cd4d8d1783a7fa50729c478a6883cfc64
Daniel Ehrlich <daniel.ehrlich1@gmail.com>
2016-10-18 23:40:39 -0500

    Create JS folder and added README

--- hello-world.js
+++
@@ -1,3 +0,0 @@
-//Just press F12 (Chrome, Firefox), select the "Console" tab and type:
-//In a nodejs environment you can run the code using "node hello_world.js" from the repository directory
-console.log("Hello World!"); //Voilà!
